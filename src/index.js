ba4923d9fbef19dbe56c513cea451ddfb69cac0b
kcotias <kaianexperia@gmail.com>
2019-04-13 02:13:38 -0300

    add props system to commentdetail

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+
 import CommentDetail from './components/CommentDetail';
 
 const App = () => {
     return(
         <div className="ui container comments">
-            <CommentDetail/>
+            <CommentDetail
+                author="Kaian"
+                timeAgo="Today at 2:00AM"
+                comment="I know that im gay"
+                avatar={faker.image.avatar()}
+            />
         </div>
     );
 }
